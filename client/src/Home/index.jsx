eee8c08b84c544fb931cbd65b4b01be118cc2030
nathyyy06 <nathalia.cruz@estudante.ifms.edu.br>
2024-08-24 11:29:56 -0400

    banco de dados

--- client/src/Home/index.jsx
+++ client/src/Home/index.jsx
@@ -7,13 +7,13 @@ export default function Home() {
             <h2>Sistema Acadêmico</h2>
             <div className="card-container">
                 <Link to="/matricula/cadastrar" className="card">
-                    <div>Registrar Matrícula</div>
+                    <div>Cadastrar conta</div>
                 </Link>
                 <Link to="/matriculas" className="card">
-                    <div>Lista de Matrícula</div>
+                    <div>Listar login</div>
                 </Link>
                 <Link to="/matriculas/alterar" className="card">
-                    <div>Editar Matrícula</div>
+                    <div>Editar login</div>
                 </Link>
             </div>
         </div>
