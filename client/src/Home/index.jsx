68e28f91e5a11617c3dce6bb8c2a8471790ac3a2
nathyyy06 <nathalia.cruz@estudante.ifms.edu.br>
2024-09-02 10:08:47 -0400

    entrega do trabalho

--- client/src/Home/index.jsx
+++ client/src/Home/index.jsx
@@ -2,28 +2,53 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../globals.css';
 
+
+
+
+
+
 export default function Home() {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = () => {
-        setIsOpen(!isOpen);
+        setIsOpen(prevState => !prevState);
     };
 
     return (
-        <div className="container">
-            <button className="menu-toggle" onClick={toggleMenu}>
-                {isOpen ? 'Fechar Menu' : 'Abrir Menu'}
-            </button>
+        
+       
             <div className={`sidebar ${isOpen ? 'open' : ''}`}>
                 <h3>Menu</h3>
-                <ul class="nav-list">
-    <li><Link to="/netflix/cadastrar"><i class="bi bi-person-lines-fill"></i>Cadastrar conta</Link></li>
-    <li><Link to="/netflix"><i class="bi bi-people-fill"></i>Contas cadastradas</Link></li>
-    <li><Link to="/netflix/alterar"><i class="bi bi-pencil-square"></i>Editar conta</Link></li>
-</ul>
-            </div>
+                <div className="sidebar">
+                    <ul>
+                        <li>
+                            <Link to="/netflix/cadastrar">
+                                <span className="icon">
+                                    <i className="bi bi-person-lines-fill"></i>
+                                </span>
+                                <span className="text">Cadastrar conta</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/netflix">
+                                <span className="icon">
+                                    <i className="bi bi-people-fill"></i>
+                                </span>
+                                <span className="text">Contas cadastradas</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="/netflix/alterar">
+                                <span className="icon">
+                                    <i className="bi bi-pencil-square"></i>
+                                </span>
+                                <span className="text">Editar conta</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
             
-            
+
     );
 }
